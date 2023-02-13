77d746fe1861679795fa7d5b5f557b305b1bf19a
ashishbhatarkar <ashu.bhatarkar@gmail.com>
2023-02-13 19:09:35 +0530

    Search fun & main fun

---
+++ LockedMe/Mainfun.java
@@ -0,0 +1,10 @@
+package LockedMe;
+
+public class Mainfun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- LockedMe/Operations.java
+++ LockedMe/Operations.java
@@ -7,24 +7,34 @@ public class Operations {
 	
 	Scanner sc = new Scanner(System.in);
 	
-	public void add()throws Exception 
-	{
+	public void add()throws Exception {
 		System.out.println("Enter File Name to Add");
 		String fnme = sc.nextLine();
 		
 		File adf = new File(fnme);
 			
-			if(adf.exists())
-			{
+			if(adf.exists()) {
 				System.out.println("File Already Exits");
 			}
 			
-			else
-			{
+			else {
 				adf.createNewFile();
 				System.out.println( fnme + "File Created");
 			}
-		
+	}
+	public void deletefile() {
+		System.out.println("Enter File Name to Delete");
+		String fnme = sc.nextLine();
+		File def = new File(fnme);
+			
+			if(def.delete())
+				System.out.println("File has Deleted");
+			else
+				System.out.println("File Not Fount");	
+	}
+	
+	public void searchf() {
+		File dty = new File("C:\\Users\\Darsh\\Project\\src\\Content Files");
 		
 		
 	}
