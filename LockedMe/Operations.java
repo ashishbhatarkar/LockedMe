ff6a757eae286b3bb78d6a6a443403b2db9a250a
ashishbhatarkar <ashu.bhatarkar@gmail.com>
2023-02-13 18:46:22 +0530

    Adding Prog

--- LockedMe/Operations.java
+++ LockedMe/Operations.java
@@ -1,5 +1,32 @@
 package LockedMe;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Operations {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	public void add() 
+	{
+		System.out.println("Enter File Name to Add");
+		String fnme = sc.nextLine();
+		
+		File adf = new File(fnme);
+			
+			if(adf.exists())
+			{
+				System.out.println("File Already Exits");
+			}
+			
+			else
+			{
+				adf.createNewFile();
+				System.out.println( fnme + "File Created");
+			}
+		
+		
+		
+	}
 
 }
