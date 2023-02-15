bb0e9a8a20992bced476a95a4cfc479fa0338286
ashishbhatarkar <ashu.bhatarkar@gmail.com>
2023-02-15 13:10:39 +0530

    Some Changes in ShowFile program

--- LockedMe/ShowFile.java
+++ LockedMe/ShowFile.java
@@ -1,5 +1,28 @@
 package LockedMe;
 
-public class ShowFile {
+import java.io.File;
+import java.util.*;
+import LockedMe.lpOperation;
 
+public class ShowFile {
+	
+	
+	public static Set<String> getFiles(){
+		File[] files = new File("E:\\Files\\").listFiles();
+		
+		Set<String> sorted = new TreeSet<>();
+		
+		for(File file: files) {
+			sorted.add(file.getName());
+		}
+		return(sorted);
+		
+	}
+	
+	public static void ShowFiles() {
+		
+		System.out.println("Show Files In Ascending order");
+		getFiles().forEach(System.out::println);
+		System.out.println("-------------------");
+	}
 }
